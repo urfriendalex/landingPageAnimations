189fc459d9556ac98bbfbc735724d9940aea2f9d
aliaksandr.yansans <smizer_fun@mail.ru>
2019-07-31 18:32:10 +0200

    Added more animations

--- js/index.js
+++ js/index.js
@@ -1,73 +1,129 @@
 const hero = document.querySelector('.hero')
 const slider = document.querySelector('.slider')
 const logo = document.querySelector('#logo')
-const hamburger = document.querySelector('#hamburger')
+const hamburger = document.querySelector('.hamburger')
 
-const hamburger1 = document.querySelector('#hamburger span:nth-of-type(1)')
-const hamburger2 = document.querySelector('#hamburger span:nth-of-type(2)')
-const hamburger3 = document.querySelector('#hamburger span:nth-of-type(3)')
+const hamburger1 = document.querySelector('.hamburger span:nth-of-type(1)')
+const hamburger2 = document.querySelector('.hamburger span:nth-of-type(2)')
+const hamburger3 = document.querySelector('.hamburger span:nth-of-type(3)')
 
 const headline = document.querySelector('.headline')
 
-const tl = new TimelineMax();
+const navslider = document.querySelector('.nav-slider')
 
-tl.fromTo(
-    slider,
-    1,
-    { width: '0'},
-    { width: '100%', ease: Power2.easeInOut}
-    )
-    .fromTo(
-    hero,
-    1.2,
-    {height: '0%'},
-    {height: '80%', ease: Power2.easeInOut}
+const navlinks = document.querySelector('.nav-links')
+
+const socailMedia = document.querySelector('.social-media')
+
+const nextArrow = document.querySelector('.next-arrow')
+
+const tl1 = new TimelineMax();
+const tl2 = new TimelineMax({paused:true});
+
+
+tl1.fromTo(
+        slider,
+        1,
+        { width: '0'},
+        { width: '100%', ease: Power2.easeInOut}
     )
     .fromTo(
         hero,
-        1,
-        {wdth: '100%'},
-        {width: '70%',
-        ease: Power2.easeInOut} 
+        .8,
+        {height: '0%'},
+        {height: '80%', ease: Power2.easeInOut}
     )
     .fromTo(
         logo,
-        1,
+        .5,
         {opacity: '0'},
         {opacity: '1',
         ease: Power2.easeInOut} 
     )
     .fromTo(
         headline,
-        1,
+        .5,
         {opacity: '0'},
         {opacity: '1',
         ease: Power2.easeInOut},
-        "-=1" 
+        "-=0.5" 
     )
     .fromTo(
         hamburger1,
-        .3,
-        {width: '0'},
-        {width: '36px',
-        ease: Power2.easeInOut, clearProps﻿:"width"}
+        .1,
+        {width: '0', border: '0'},
+        {width: '36px', border: '1px solid white',
+        ease: Power2.easeInOut, clearProps: "width, border"}
     )
     .fromTo(
         hamburger2,
-        .3,
-        {width: '0'},
-        {width: '26px',
-        ease: Power2.easeInOut, clearProps﻿:"width"}
+        .2,
+        {width: '0', border: '0'},
+        {width: '26px', border: '1px solid white',
+        ease: Power2.easeInOut, clearProps: "width, border"}
     )
     .fromTo(
         hamburger3,
-        .3,
-        {width: '0'},
-        {width: '20px',
-        ease: Power2.easeInOut, clearProps﻿:"width"}
+        .2,
+        {width: '0', border: '0'},
+        {width: '20px', border: '1px solid white',
+        ease: Power2.easeInOut, clearProps: "width, border"}
     );
 
 
-function test(){
-    console.log('test');
-}
+    tl2.fromTo(
+            navslider,
+            0.1,
+            {display: "none"},
+            {display: "flex"}
+        ).fromTo(
+            navslider,
+            0.8,
+            {right: "-30%"},
+            {right: "0"}
+        ).fromTo(
+            navlinks.children[0],
+            0.2,
+            {right: "-100%"},
+            {right: "0"}
+        ).fromTo(
+            navlinks.children[1],
+            0.2,
+            {right: "-100%"},
+            {right: "0"}
+        ).fromTo(
+            navlinks.children[2],
+            0.2,
+            {right: "-100%"},
+            {right: "0"}
+        ).fromTo(
+            navlinks.children[3],
+            0.2,
+            {right: "-100%"},
+            {right: "0"}
+        ).fromTo(
+            socailMedia,
+            0.4,
+            {bottom: "-100%"},
+            {bottom: "0"}
+        )
+
+hamburger.addEventListener("click",function(){
+    if (!hamburger.classList.contains("active")){
+        hamburger.classList.add("active");
+        tl2.play();
+    }
+    else
+    {
+        hamburger.classList.remove("active");
+        tl2.reverse();
+    }
+})
+
+const tl3 = new TimelineMax({paused:true, repeat:1, yoyo:true});
+
+
+
+  nextArrow.addEventListener("click",function(){
+      tl3.play();
+  });
